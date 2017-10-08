a8562647da4a16a2454fe65c29e9635c27158e2a
vuleenguyen <vuleenguyen.92@gmail.com>
2017-10-08 10:51:46 -0700

    'djkasjkdjksajkdjkaskjd'

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    //private final RecipeService recipeService;
+    private final RecipeService recipeService;
 
     public IndexController(RecipeService recipeService) {
         this.recipeService = recipeService;
